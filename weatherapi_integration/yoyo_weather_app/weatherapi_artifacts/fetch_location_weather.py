907b6e76fdd6d5072e977a8ad926d99513b80cd4
Psycadelik <francisadrian9@hotmail.com>
2021-09-05 00:11:37 +0300

    fetch api key from django settings

--- weatherapi_integration/yoyo_weather_app/weatherapi_artifacts/fetch_location_weather.py
+++ weatherapi_integration/yoyo_weather_app/weatherapi_artifacts/fetch_location_weather.py
@@ -1,11 +1,11 @@
 import requests
-api_key = "fc5acdf1e9d740718f591743210209"
+from django.conf import settings
 
 
 def fetch_location_weather_data(location, number_of_days):
     # url to make request to
     res = requests.get("https://api.weatherapi.com/v1/forecast.json?key=" \
-          "{}&q={}&days={}&aqi=no&alerts=no".format(api_key, location,
+          "{}&q={}&days={}&aqi=no&alerts=no".format(settings.API_KEY, location,
                                                     number_of_days))
 
     if res.status_code == 200:
