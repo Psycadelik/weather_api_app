d90896b99c9df0d283c68de1529d891e8837e12d
Psycadelik <francisadrian9@hotmail.com>
2021-09-05 00:06:19 +0300

    add api key to settings

--- weatherapi_integration/weatherapi_integration/settings.py
+++ weatherapi_integration/weatherapi_integration/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# API key for weatherapi.com
+API_KEY = os.getenv('<your-api-key>', 'fc5acdf1e9d740718f591743210209')
